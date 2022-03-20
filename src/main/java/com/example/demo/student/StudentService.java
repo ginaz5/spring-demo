24c0e6d3b96e649481a9a1292cc76d676d78fab7
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-03-20 16:16:16 +0800

    add DELETE method

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -31,4 +31,12 @@ public class StudentService {
         }
         studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " doesn't exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
 }
