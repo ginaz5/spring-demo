6a9e798dc18fc3dbd6837bc20ef3bd281116bf3d
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-03-20 23:56:29 +0800

    add PUT method and README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+a simple spring boot project with RESTful API
+
+- Spring framework
+- JPA
+- Optional 
+
+StudentController.java
+Controller
+
+StudentService.java
+implementation 
+
+
+RESTful
+GET - get student info from database
+POST - add a student to database
+DELETE - delete a student from database by given id
+PUT - update student
+
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -38,5 +40,24 @@ public class StudentService {
             throw new IllegalStateException("student with id " + studentId + " doesn't exists");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException (
+                        "student with id "+ studentId + "doesn't existes"
+                ));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
