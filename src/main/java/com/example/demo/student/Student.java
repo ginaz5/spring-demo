9aac93dc59b9d48c4aee14dafb244c95d9c8bf74
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-03-20 15:33:02 +0800

    add POST method

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,6 +2,8 @@ package com.example.demo.student;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 
 @Entity // Hibernate
 @Table // this is for table in database
@@ -20,6 +22,7 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+    @Transient
     private Integer age;
 
     public Student(){
@@ -28,23 +31,19 @@ public class Student {
     public Student(Long id,
                    String name,
                    String email,
-                   LocalDate dob,
-                   Integer age) {
+                   LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Student(String name,
                    String email,
-                   LocalDate dob,
-                   Integer age) {
+                   LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -75,12 +74,12 @@ public class Student {
         return dob;
     }
 
-    public void setDob(LocalDate dob) {
+    public void setDob(LocalDate dob)  {
         this.dob = dob;
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
